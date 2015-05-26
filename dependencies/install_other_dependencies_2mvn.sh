135a61a9fa7aa29beade63474fd1ee765188f99d
bozyurt <iozyurt@ucsd.edu>
2015-05-26 13:12:29 -0700

    changes

--- dependencies/install_other_dependencies_2mvn.sh
+++ dependencies/install_other_dependencies_2mvn.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
 
-mvn install:install-file -Dfile=$HOME/dev/resource_disambiguator/dependencies/lib/opennlp-maxent-3.0.3.jar -DgroupId=bnlp -DartifactId=opennlp-maxent -Dversion=3.0.3 -Dpackaging=jar
-
-mvn install:install-file -Dfile=$HOME/dev/resource_disambiguator/dependencies/lib/opennlp-tools-1.5.3.jar -DgroupId=bnlp -DartifactId=opennlp-tools -Dversion=1.5.3 -Dpackaging=jar
-
 mvn install:install-file -Dfile=$HOME/dev/resource_disambiguator/dependencies/lib/jsvmlight.jar -DgroupId=bnlp -DartifactId=jsvmlight -Dversion=0.1 -Dpackaging=jar
 
--- dependencies/install_stanford_nlp_2mvn.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-
-mvn install:install-file -Dfile=$HOME/dev/resource_disambiguator/dependencies/lib/stanford-corenlp-1.3.4.jar -DgroupId=edu.stanford.nlp -DartifactId=stanford-corenlp -Dversion=1.3.4 -Dpackaging=jar
-
-mvn install:install-file -Dfile=$HOME/dev/resource_disambiguator/dependencies/lib/stanford-corenlp-models-1.3.4.jar -DgroupId=edu.stanford.nlp -DartifactId=stanford-corenlp-models -Dversion=1.3.4 -Dpackaging=jar
-
-
